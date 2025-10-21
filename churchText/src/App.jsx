f86cad9659ac82deb1bd407fafae7a847cdacbcc
AGUEH Barnabé Prudent Mahugnon <111515950+Barnab1@users.noreply.github.com>
2025-10-21 19:45:23 +0000

    I started the app with one sunday data. It worked

---
+++ churchText/src/App.jsx
@@ -0,0 +1,76 @@
+import sundayText from './assets/sundayText';
+import {useState} from 'react';
+/**
+ * Display the fixed top buttons
+ */
+const Header = ({eventDate, eventLecture})=>{
+  return (
+    <div>
+      <form>
+        <label htmlFor='date'>Date: </label>
+        <input type="date" id='date' onChange= {eventDate} />
+      </form>
+
+      <div>
+        <button className="btn" onClick={() => eventLecture('lecture1')}>Lecture 1</button>
+        <button className="btn" onClick={() => eventLecture('lecture2')}>Lecture 2</button>
+        <button className="btn" onClick={() => eventLecture('evangile')}>Evangile</button>
+        <button className="btn" onClick={() => eventLecture('commentaire')}>Commentaire</button>
+      </div>
+    </div>
+  )
+}
+
+
+function App() {
+  
+  const [error, setError] = useState('');
+  const [firstLecture, setFirstLecture] = useState('');
+  const [secondLecture, setSecondLecture] = useState('');
+  const [evangile, setEvangile] = useState('');
+  const [commentaire, setCommentaire] = useState('');
+  const [lecture, setLecture] = useState('');
+
+
+  const handleDateChange = (date)=>{
+    console.log(date);
+    
+    let dayText = sundayText.find(sunday=> sunday.date === date);
+    if(dayText){
+     setFirstLecture(dayText.lectures.lecture1);
+     setSecondLecture(dayText.lectures.lecture2);
+     setEvangile(dayText.lectures.evangile);
+     setCommentaire(dayText.lectures.commentaire);
+     setLecture(dayText.lectures.lecture1) ;
+     setError('');
+    }else{
+      setError('Aucun texte disponible pour cette date');
+      setFirstLecture('');
+      setSecondLecture('');
+      setEvangile('');
+      setCommentaire('');
+      setLecture('');
+    }
+
+  } 
+
+  return (
+    <div>
+      <Header eventDate={(e)=> handleDateChange(e.target.value)} eventLecture={(val)=>{
+        val === 'lecture1' && setLecture(firstLecture)
+        val === 'lecture2' && setLecture(secondLecture)
+        val === 'evangile' && setLecture(evangile)
+        val === 'commentaire' && setLecture(commentaire)
+        
+      }}/>
+
+        <div>
+          {lecture}
+          {error && <p>{error}</p>}
+        </div>
+    </div>
+
+  )
+}
+
+export default App
